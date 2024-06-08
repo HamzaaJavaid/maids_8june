3af8143dc020368c747694da69ec80cdf54cc359
Hamza Javaid <humjavaid@gmail.com>
2024-06-09 00:37:16 +0500

    Utiliizing Application Memory , Increase Efficiency

--- lib/modals_variables/modals_var_info.dart
+++ lib/modals_variables/modals_var_info.dart
@@ -1,6 +1,10 @@
 class vm_var{
 
 
+
+ //avoiding models because in REST_API structure it takes more space and memory hence only using view_model variables strategy
+ //to over come this problem HENCE smoth running of app using less resources
+ 
  static  var id;
  static var name ;
  static var email;
